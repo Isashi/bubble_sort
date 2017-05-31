b195b40e6532f536d38f1e6ac5525d0df061e7d2
Isashi <isashimitsui@gmail.com>
2017-05-31 16:11:48 +0200

    add bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,11 +3,27 @@ def bubble_sort(array)
 	n = array.length
 	until n==1
 		for i in 0..n-2 do
-			puts "cycle #{(n-1).to_s} - loop #{(i+1).to_s}: #{array.join(" - ").to_s}"
 			if array[i] > array[i+1] then array[i], array[i+1] = array[i+1], array[i] end
+			puts "cycle #{(n-1).to_s} - loop #{(i+1).to_s}: #{array.join(" - ").to_s}"
+		end
+		n -= 1
+	end
+end
+
+def bubble_sort_by(array)
+	puts array.join(" - ")
+	n = array.length
+	until n==1
+		for i in 0..n-2 do
+			if yield(array[i],array[i+1])>0 then array[i], array[i+1] = array[i+1], array[i] end
+			puts "cycle #{(n-1).to_s} - loop #{(i+1).to_s}: #{array.join(" - ").to_s}"	
 		end
 		n -= 1
 	end
 end
 
 bubble_sort([1, 100, 4,3,78,2,0,2, 0])
+
+bubble_sort_by(["hi","hello","hey","prova","a"]) do |left,right|
+	left.length - right.length
+end
